40dcd109413e67b078661d79f1dbbabb6688f00c
Francois <francoistambwe@gmail.com>
2022-01-24 14:08:37 +0100

    stuff

---
+++ app/Http/Controllers/Admin/AdminProductController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class AdminProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $data = Product::all();
+        return view('admin.products.index',compact('data'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        return view('layouts.form');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Product $product
+     * @return Response
+     */
+    public function show(Product $product)
+    {
+        return view('admin.products.show',compact('product'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param Product $product
+     * @return Response
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param Product $product
+     * @return Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param Product $product
+     * @return Response
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
---
+++ resources/views/admin/products/edit.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+
+@section('title', __('Edit Products'))
+@section('header', __('Edit Products'))
+
+@section('content')
+    <div>
+        <x-form action="">
+
+        </x-form>
+
+    </div>
+
+@endsection
