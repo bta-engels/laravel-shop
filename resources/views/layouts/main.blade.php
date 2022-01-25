f0ba42e26f262818779047e790f668ccd3157127
berndengels <engels@f50.de>
2022-01-25 11:30:17 +0100

    stuff

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -29,7 +29,11 @@
         </div>
         <div>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                bla bla
+                @auth
+                    {{ auth()->user()->name }}
+                @else
+                    public
+                @endauth
             </h2>
         </div>
 
