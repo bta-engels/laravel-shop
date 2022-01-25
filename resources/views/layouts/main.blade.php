889a9b81dbb14290082bc75c7c06c44d50607005
berndengels <engels@f50.de>
2022-01-25 10:26:43 +0100

    stuff

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -21,15 +21,22 @@
 @endauth
 
 <!-- Page Heading -->
-<header class="bg-white shadow">
-    <div class="">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            @section('header')
-                Unser Header<br>
-            @show
-        </h2>
-    </div>
-</header>
+    <header>
+        <div>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                @section('header')
+                    Unser Header<br>
+                @show
+            </h2>
+        </div>
+        <div>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                bla bla
+            </h2>
+        </div>
+
+    </header>
+    <div class="table_content">
 
 <div class="content">
     <!-- Page Content -->
