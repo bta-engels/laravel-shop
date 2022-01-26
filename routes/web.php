6094f9460523df63999f01ed13d96efaff6b2848
Francois <francoistambwe@gmail.com>
2022-01-26 12:44:38 +0100

    stuff

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/', function () {
 });
 
 Route::get('lang/{lang}', function ($lang){
+    session()->put('language', $lang);
     return redirect()->back();
 })->name('lang.switch');
 
