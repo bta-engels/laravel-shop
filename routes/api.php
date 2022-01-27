7ae50fb009f1853e6967fd9c6d5a732a71dcf705
berndengels <engels@f50.de>
2022-01-27 13:47:04 +0100

    stuff

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ApiLoginController;
 use App\Http\Controllers\Api\ApiTodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,5 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('login', [ApiLoginController::class, 'login']);
 
 Route::apiResource('todos', ApiTodoController::class);
