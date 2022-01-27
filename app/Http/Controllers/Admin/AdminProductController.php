b47b7f87cee604c13eff46a963b0a303a55f9541
bakrihamza <bakrihamza2209@gmail.com>
2022-01-27 14:01:58 +0100

    stuff

--- app/Http/Controllers/Admin/AdminProductController.php
+++ app/Http/Controllers/Admin/AdminProductController.php
@@ -20,7 +20,7 @@ class AdminProductController extends Controller
      */
     public function index()
     {
-        $data = Product::all();
+        $data = Product::orderBy('name')->paginate(5);
         return view('admin.products.index',compact('data'));
     }
 
--- app/Models/Feature.php
+++ app/Models/Feature.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
+use Database\Factories\FeatureFactory;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\Models\Feature
+ *
+ * @property int $id
+ * @property string $title
+ * @property string|null $body
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static FeatureFactory factory(...$parameters)
+ * @method static Builder|Feature newModelQuery()
+ * @method static Builder|Feature newQuery()
+ * @method static Builder|Feature query()
+ * @method static Builder|Feature whereBody($value)
+ * @method static Builder|Feature whereCreatedAt($value)
+ * @method static Builder|Feature whereId($value)
+ * @method static Builder|Feature whereTitle($value)
+ * @method static Builder|Feature whereUpdatedAt($value)
+ * @mixin Eloquent
+ */
 class Feature extends Model
 {
     use HasFactory;
