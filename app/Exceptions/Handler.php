9a7e0f1280f227a8d44c67e1c0eae3e610955ed2
berndengels <engels@f50.de>
2022-02-01 13:19:20 +0100

    disable cache

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,8 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+//use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Bengels\LaravelEmailExceptions\Exceptions\EmailHandler as ExceptionHandler;
 use Throwable;
 
 class Handler extends ExceptionHandler
